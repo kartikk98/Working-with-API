5971b2507f444e4e0e393ee8993a2f457284e4eb
Kartik Kumar <36085692+kartikk98@users.noreply.github.com>
2019-06-16 01:20:18 +0530

    Uploaded files

--- influencers.js
+++ influencers.js
@@ -1,35 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="styles.css">
-    <title>Influencers</title>
-  </head>
+var section = document.querySelector('section');
 
-
-
-  <body>
-    
-      
-      <section>
-
-      </section>
-
-    
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-   
-
-    
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-
-  <script type="text/javascript">
-    
-      var section = document.querySelector('section');
 var requestURL = "http://demo4469839.mockable.io/influencers"
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -92,6 +62,3 @@ function showInfluencers(jsonObj){
 
   }
 }
-
-  </script>
-</html>
